91703fa46132faa7ed1c78840feffca7d9c1d8aa
AdamProtocol <78094177+AdamProtocol@users.noreply.github.com>
2022-02-16 21:12:57 +0800

    Add files via upload

---
+++ R_using_telnet.py
@@ -0,0 +1,21 @@
+#using telnet
+
+import telnetlib
+import time
+
+#show how telnet work manualy
+
+host = '10.1.1.4'
+
+tn = telnetlib.Telnet(host)
+
+tn.write(b'pass \n')
+tn.write(b'enable \n')
+tn.write(b'config t \n')
+tn.write(b'hostname telnetpythonSwitch-11 \n')
+tn.write(b'end \n')
+tn.writeb('exit \n')
+
+read_output = tn.read_all().decode('ascii')
+print(read_output)
+

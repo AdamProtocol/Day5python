da85f01e5b6c4906d6145a819f94142fcb6762b4
AdamProtocol <78094177+AdamProtocol@users.noreply.github.com>
2022-02-18 22:38:24 +0800

    Add files via upload

--- Rivan_telnet_wjson.py
+++ Rivan_telnet_wjson.py
@@ -4,31 +4,31 @@ import json
 import telnetlib
 
 with open('Rivan_devices.json') as f:
-	buksan_file = json.load(f)
+    buksan_file = json.load(f)
 
 
-print(buksan_file[Student_11])
-print(buksan_file[Student_11][switch_ip_vlan1])
+#print(buksan_file['Student_11'])
+#print(buksan_file['Student_11']['switch_ip_vlan1'])
 
 #if hirap pa sila intindihin ung concept make few more exercises
 
 def login():
-	tn.write(b'pass \n')
-	tn.write(b'enable \n')
+    tn.write(b'pass \n')
+    tn.write(b'enable \n')
 
 def interface_shut(port_num):
-	tn.write(b'config t \n')
-	tn.write(b'interface '+port_num.encode('ascii')+b'\n')
-	tn.write(b'shutdown \n')
+    tn.write(b'config t \n')
+    tn.write(b'interface '+port_num.encode('ascii')+b'\n')
+    tn.write(b'shutdown \n')
 
 def end_sess():
-	tn.write(b'end \n')
-	tn.write(b'exit \n')
+    tn.write(b'end \n')
+    tn.write(b'exit \n')
 
 def get_output():
-	tn.close()
-	read_output = tn.read_all.decode('ascii')
-	print(read_output)
+    tn.close()
+    read_output = tn.read_all().decode('ascii')
+    print(read_output)
 
 def initconfigdev(hostname,passw):
         command = [
@@ -55,43 +55,43 @@ def initconfigdev(hostname,passw):
         print('Initialized')
 
 def pick_stud(num):
-	stud = buksan_file[f'Student_{num}']
-	return stud
+    stud = buksan_file[f'Student_{num}']
+    return stud
 
 
 def config_vlans(stud):
-	mask = '255.255.255.0'
-	command = [
-		'enable',
-		'configure terminal',
-		'interface vlan 1',
-		'no shutdown',
-		f'ip address {stud[switch_ip_vlan1]} {mask}',
-		'description mgmtvlan',
-		'interface vlan 1',
-		'no shutdown',
-		f'ip address {stud[switch_ip_vlan10]} {mask}',
-		'description wirelessvlan',
-		'interface vlan 1',
-		'no shutdown',
-		f'ip address {stud[switch_ip_vlan100]} {mask}',
-		'description voicevlan'
+    mask = '255.255.255.0'
+    command = [
+        'enable',
+        'configure terminal',
+        'interface vlan 1',
+        'no shutdown',
+        f'ip address {stud["switch_ip_vlan1"]} {mask}',
+        'description mgmtvlan',
+        'interface vlan 1',
+        'no shutdown',
+        f'ip address {stud["switch_ip_vlan10"]} {mask}',
+        'description wirelessvlan',
+        'interface vlan 1',
+        'no shutdown',
+        f'ip address {stud["switch_ip_vlan100"]} {mask}',
+        'description voicevlan'
 
-	]
+    ]
 
-	for i in command:
-		#concept: you can put words infront and at the back
-		i = i[:0] + "b'" + i[0:] +r"\n'"
-		tn.write(i)
+    for i in command:
+        #concept: you can put words infront and at the back
+        i = i[:0] + "b'" + i[0:] +r"\n'"
+        tn.write(i)
 
 
 #executing the app
 host = pick_stud('11')
 #10.11.1.4
-tn = telnetlib.Telnet(host[switch_ip_vlan1])
+tn = telnetlib.Telnet(host["switch_ip_vlan1"])
 login()
 initconfigdev('myname_switch', 'cisco')
-config_vlan(pick_stud(11))
+config_vlans(pick_stud(11))
 end_sess()
 get_output()
 
